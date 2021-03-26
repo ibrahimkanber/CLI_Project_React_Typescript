8a005e21800a14bce3a1c3267968a3b8908a0500
ibrahim <ikanber45@gmail.com>
2021-03-26 14:32:14 +0100

    code splitting

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from "esbuild-wasm"
 import ReactDOM from "react-dom"
 import { useEffect, useState, useRef } from "react"
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin"
+import { fetchPlugin } from "./plugins/fetch-plugin"
 
 const App = () => {
     const ref = useRef<any>()
@@ -11,7 +12,7 @@ const App = () => {
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: "/esbuild.wasm"
+            wasmURL: "http://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
         })
 
     }
@@ -30,7 +31,7 @@ const App = () => {
             entryPoints: ["index.js"],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin(input)],
+            plugins: [unpkgPathPlugin(),fetchPlugin(input)],
             define:{
                 'process.env.NODE_ENV':' "production" ',
                 global:"window"
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,11 +1,4 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios'
-
-import localforage from "localforage"
-
-const fileCache=localforage.createInstance({
-    name:"filecache"
-});
 
 /* (async()=>{
   await fileCache.setItem("color","red");
@@ -14,33 +7,30 @@ const fileCache=localforage.createInstance({
   console.log(color)
 })() */
 
-export const unpkgPathPlugin = (inputCode:string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
+
     setup(build: esbuild.PluginBuild) {
+
+    //Handle root entry file of index.js
+
+      build.onResolve({filter:/(^index\.js$)/},()=>{
+        return {path:"index.js",namespace:"a"}
+      })
+   //Handle relative paths in module
+
+      build.onResolve({filter:/^\.+\//},(args:any)=>{
+        return {
+          namespace: "a",
+          //path: new URL(args.path, args.importer+"/").href
+          path: new URL(args.path, 'https://unpkg.com'+args.resolveDir+"/").href
+        }
+      })
+
+ //Handle main file of a module
+
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        if (args.path === "index.js") {
-
-          return {
-
-            path: args.path,
-
-            namespace: 'a'
-          };
-
-        }
-
-        if (args.path.includes("./") || args.path.includes("../")) {
-         /*  console.log("path:", args.path)
-          console.log("url==>", new URL(args.path, args.importer+"/")) */
-          return {
-            namespace: "a",
-            //path: new URL(args.path, args.importer+"/").href
-            path: new URL(args.path, 'https://unpkg.com'+args.resolveDir+"/").href
-          }
-        }
-
 
         return {
 
@@ -49,45 +39,9 @@ export const unpkgPathPlugin = (inputCode:string) => {
           path: `https://unpkg.com/${args.path}`
 
         }
-
-        /* else if(args.path==="tiny-test-pkg"){
-          return{
-            path:'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-            namespace:"a"
-          }
-        } */
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
-        const cachedresult=await fileCache.getItem<esbuild.OnLoadResult>(args.path)
-
-        if(cachedresult){
-          return cachedresult
-        }
-
-
-        const { data ,request} = await axios.get(args.path)
-        //console.log(request)
-        const result:esbuild.OnLoadResult= {
-          loader: "jsx",
-          contents: data,
-          resolveDir:new URL("./",request.responseURL).pathname
-        }
-
-        await fileCache.setItem(args.path,result)
-
-        return result
-
-      });
+  
     },
   };
 };
