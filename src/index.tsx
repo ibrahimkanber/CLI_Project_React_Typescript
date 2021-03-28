d567b9a175301196dad9acabe53365df99862ac7
ibrahim <ikanber45@gmail.com>
2021-03-28 23:23:31 +0200

    editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import * as esbuild from "esbuild-wasm"
 import ReactDOM from "react-dom"
 import { useEffect, useState, useRef } from "react"
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin"
 import { fetchPlugin } from "./plugins/fetch-plugin"
+import CodeEditor from "./components/code-editor"
 
 const App = () => {
     const ref = useRef<any>()
+    const iframe = useRef<any>()
     const [input, setInput] = useState("")
-    const [code, setCode] = useState("")
+
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -27,20 +30,29 @@ const App = () => {
                 loader:'jsx',
                 target:'es2015'
             }) */
+
+        iframe.current.srcdoc = html
+
         const result = await ref.current.build({
             entryPoints: ["index.js"],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin(),fetchPlugin(input)],
-            define:{
-                'process.env.NODE_ENV':' "production" ',
-                global:"window"
+            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+            define: {
+                'process.env.NODE_ENV': ' "production" ',
+                global: "window"
             }
         })
 
-        console.log(result)
-        setCode(result.outputFiles[0].text)
-
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*")
+        //console.log(result)
+        //setCode(result.outputFiles[0].text)
+        /*      try {
+                 eval(result.outputFiles[0].text);
+     
+             } catch (error) {
+                 alert(error)
+             } */
     }
 
     useEffect(() => {
@@ -48,15 +60,48 @@ const App = () => {
     }, [])
 
 
-    return <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+    const html = `
+    
+    <html>
+    <head></head>
+    <body>
+        <div id="root"></div>
+        <script>
+        window.addEventListener("message",(event)=>{
+            try{
+                eval(event.data)
+            }catch(err){
+                const root=document.querySelector("#root");
+                root.innerHTML='<div style="color:red"><h4>Runtime Error</h4>'+err+'</div>'
+                throw err
+            }
+        
+
+        },false)
+        </script>
+    </body>
+    
+    </html>
+    `
+    return (
         <div>
-            <button onClick={onClick}>
-                Submit
+            <CodeEditor 
+            initialValue="const a=1;"
+            onChange={val=>setInput(val)}
+            
+            />
+            <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+            <div>
+                <div>
+                    <button onClick={onClick}>
+                        Submit
             </button>
-            <pre>{code}</pre>
+                </div>
+
+                <iframe sandbox="allow-scripts" srcDoc={html} ref={iframe} title="code preview" />
+            </div>
         </div>
-    </div>
+    )
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"))
