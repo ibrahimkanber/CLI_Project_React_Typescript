e239177d521c8eec58637aa0a372f1042afbba3b
ibrahim <ikanber45@gmail.com>
2021-03-29 12:49:56 +0200

    error handling bundle error and runtime error

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import CodeEditor from "./code-editor"
+import Preview from "./preview"
+import bundle from "../bundler"
+import Resizable from "./resizable"
+
+
+const CodeCell = () => {
+
+    const [code, setCode] = useState("")
+    const [input, setInput] = useState("")
+    const [err,setErr]=useState("")
+
+    useEffect(()=>{
+        let timer=setTimeout(async() => {
+            const output = await bundle(input);
+            setCode(output.code)
+            setErr(output.err)
+        }, 750);
+
+        return ()=>{
+            clearTimeout(timer)
+        }
+    },[input])
+
+    return (
+        <Resizable direction="vertical">
+            <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+                <Resizable direction="horizontal">
+                    <CodeEditor
+                        initialValue="const a=1;"
+                        onChange={val => setInput(val)}
+                    />
+                </Resizable>
+                <Preview code={code} err={err} />
+            </div>
+        </Resizable>
+    )
+}
+
+export default CodeCell
--- src/index.tsx
+++ src/index.tsx
@@ -1,105 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import * as esbuild from "esbuild-wasm"
 import ReactDOM from "react-dom"
-import { useEffect, useState, useRef } from "react"
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin"
-import { fetchPlugin } from "./plugins/fetch-plugin"
-import CodeEditor from "./components/code-editor"
-
+import CodeCell from "./components/code-cell"
 const App = () => {
-    const ref = useRef<any>()
-    const iframe = useRef<any>()
-    const [input, setInput] = useState("")
 
-
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: "http://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
-        })
-
-    }
-
-
-    const onClick = async () => {
-        //console.log(input)
-        if (!ref.current) {
-            return
-        }
-        /*     const result=await ref.current.transform(input,{
-                loader:'jsx',
-                target:'es2015'
-            }) */
-
-        iframe.current.srcdoc = html
-
-        const result = await ref.current.build({
-            entryPoints: ["index.js"],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV': ' "production" ',
-                global: "window"
-            }
-        })
-
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*")
-        //console.log(result)
-        //setCode(result.outputFiles[0].text)
-        /*      try {
-                 eval(result.outputFiles[0].text);
-     
-             } catch (error) {
-                 alert(error)
-             } */
-    }
-
-    useEffect(() => {
-        startService()
-    }, [])
-
-
-    const html = `
-    
-    <html>
-    <head></head>
-    <body>
-        <div id="root"></div>
-        <script>
-        window.addEventListener("message",(event)=>{
-            try{
-                eval(event.data)
-            }catch(err){
-                const root=document.querySelector("#root");
-                root.innerHTML='<div style="color:red"><h4>Runtime Error</h4>'+err+'</div>'
-                throw err
-            }
-        
-
-        },false)
-        </script>
-    </body>
-    
-    </html>
-    `
     return (
         <div>
-            <CodeEditor 
-            initialValue="const a=1;"
-            onChange={val=>setInput(val)}
-            
-            />
-            <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-            <div>
-                <div>
-                    <button onClick={onClick}>
-                        Submit
-            </button>
-                </div>
-
-                <iframe sandbox="allow-scripts" srcDoc={html} ref={iframe} title="code preview" />
-            </div>
+            <CodeCell/>
         </div>
     )
 }
